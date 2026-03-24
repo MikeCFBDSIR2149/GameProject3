087a3544200e5ac2ea3907c93e194a26b92b73be
Mr.Yu <lfy3298@163.com>
2026-03-24 17:51:26 +0800

    菜单

---
+++ Assets/Scripts/UI/Menu/MenuSet/GameOverMenu.cs
@@ -0,0 +1,18 @@
+using UI;
+using UnityEngine;
+
+public class GameOverMenu : UIBase
+{
+    public override void UpdateUI(object data) { }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
